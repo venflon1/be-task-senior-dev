594330a35e38451a267d98c78ec93cc9fed9c6f7
roberto.amato <45068121+venflon1@users.noreply.github.com>
2022-12-20 18:52:20 +0100

    fix Spring Bean Services

--- src/main/java/com/amaris/task/handler/ReassignimentManagedTaskHandler.java
+++ src/main/java/com/amaris/task/handler/ReassignimentManagedTaskHandler.java
@@ -2,12 +2,14 @@ package com.amaris.task.handler;
 
 import javax.transaction.Transactional;
 
+import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.amaris.task.model.Employee;
 import com.amaris.task.model.Task;
 import com.amaris.task.service.impl.TaskActionServiceImpl;
 
+@Service
 public class ReassignimentManagedTaskHandler extends ManagedTaskHandler {
 	@Autowired
 	private TaskActionServiceImpl taskActionService;
@@ -21,5 +23,4 @@ public class ReassignimentManagedTaskHandler extends ManagedTaskHandler {
 	public void doExecute(final Task task, final Employee employee) {
 		this.taskActionService.manageReassignmentAction(task, employee);
 	}
-	
 }
--- src/main/java/com/amaris/task/handler/UnassignimentManagedTaskHandler.java
+++ src/main/java/com/amaris/task/handler/UnassignimentManagedTaskHandler.java
@@ -2,12 +2,14 @@ package com.amaris.task.handler;
 
 import javax.transaction.Transactional;
 
+import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.amaris.task.model.Employee;
 import com.amaris.task.model.Task;
 import com.amaris.task.service.impl.TaskActionServiceImpl;
 
+@Service
 public class UnassignimentManagedTaskHandler extends ManagedTaskHandler {
 	@Autowired
 	private TaskActionServiceImpl taskActionService;
