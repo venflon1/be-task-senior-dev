73214a892d314ebfaecca92387322bbd9bd75fe3
roberto.amato <45068121+venflon1@users.noreply.github.com>
2022-12-20 18:04:39 +0100

    adding integration test

--- src/test/java/com/amaris/task/integration/TaskIntegrationTest.java
+++ src/test/java/com/amaris/task/integration/TaskIntegrationTest.java
@@ -1,16 +1,27 @@
 package com.amaris.task.integration;
 
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import com.amaris.task.model.Task;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TaskIntegrationTest {
 	private String baseUri= "http://localhost:";
+	private static final String CONTEXT_PATH = "/tasks";
 	
 	@LocalServerPort
 	private int port;
@@ -19,9 +30,21 @@ class TaskIntegrationTest {
 
 	@BeforeEach
 	void setupUri() {
-		baseUri = baseUri.concat(String.valueOf(port));
+		baseUri = baseUri.concat(String.valueOf(port)).concat(CONTEXT_PATH);
 	}
 	
 	@Test
-	void test() {	}
+	@DisplayName(value = "SholdReturnResponseOKWithListOfTasks")
+	void getAllTasksTest() {	
+		final ResponseEntity<List<Task>> response = this.restTemplate.exchange(
+				 baseUri,
+				 HttpMethod.GET, 
+				 null, 
+				 new ParameterizedTypeReference<List<Task>>() {}
+		 );
+		
+		Assertions.assertThat(response).isNotNull();
+		Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		Assertions.assertThat(response.getBody()).isNotNull();
+	}
 }
